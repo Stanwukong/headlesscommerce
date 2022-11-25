05ef5ffd771f4eaf57a537705dfaa400deabd2cd
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-11-25 15:14:31 +0100

    Media queries

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,39 +1,60 @@
 import React from 'react';
 import Link from 'next/link';
-import { RiCompass3Line, RiBuildingLine } from 'react-icons/ri'
+import { RiCompass3Line, RiBuildingLine, RiLayoutGridFill, RiChatNewLine } from 'react-icons/ri'
+import { ImStack } from 'react-icons/im'
 
 const Navbar = () => {
 	return (
 		<div className='w-full mb-4'>
-			<div className='p-4 flex justify-between items-center'>
-				<div className=''>
-					<div className='tracking-[4px] font-[300] bg-[#fff] py-2'>
-						HEADLESS
-					</div>
-					<div className='tracking-[2px] bg-transparent mt-[-20px] -z-10'>
-						COMMERCE
+			<div className='p-4 flex items-center relative lg:pt-6'>
+				<div>
+					<div className='text-lg'>
+						<div className='font-[300] bg-[#fff]'>
+						<p className='tracking-[3px]'>HEADLESS</p>
+						</div>
+						
+						<div className='bg-transparent -mt-3'>
+						<p>COMMERCE</p>	
+						</div>
 					</div>
 				</div>
 				
-				<div className='flex justify-between text-gray-500 tracking-[0px]' >
-					<div className='flex hover:text-black mr-6'>
-						<Link className='text-2xl flex items-center mr-1' href='/'><RiCompass3Line/>
-							<div className='ml-2 text-base'>Categories</div>
-						</Link>
+				<p className='mx-7 text-4xl font-thin text-gray-300 hidden'>|</p>
+
+				<div className='flex text-gray-500 text-[15px] lg:flex' >
+
+					<div className='flex ml-3 lg:ml-'>
+						<div className='hover:text-black mr-6 hidden md:flex'>
+							<Link className=' flex items-center mr-1' href='/'><RiLayoutGridFill/>
+							<p className='ml-2'>Discover</p>
+							</Link>
+						</div>
+
+						<div className='flex hover:text-black mr-6'>
+							<Link className=' flex items-center mr-1' href='/'><RiCompass3Line/>
+								<p className='ml-2 '>Categories</p>
+							</Link>
+						</div>
+						
+						<div className='flex hover:text-black'>
+							<Link className='flex items-center mr-1' href='/'><RiBuildingLine/>
+							<p className='ml-2 '>Agencies</p>
+							</Link>
+						</div>
 					</div>
+						
+						<div className='items-center absolute right-0 hidden md:flex md:pr-3'>
+							<Link className='flex items-center hover:text-gray-900' href='/'><ImStack/><p className='ml-2'>My Stack</p></Link>
+							<Link className='flex items-center hover:text-gray-900 ml-3 py-1 px-2 bg-gray-100 rounded-md' href='/'><RiChatNewLine/><p className='ml-2'>Suggest</p></Link>
+						</div>
 					
-					<div className='flex hover:text-black'>
-						<Link className='text-2xl flex items-center mr-1' href='/'><RiBuildingLine/>
-						<div className='ml-2 text-base'>Agencies</div>
-						</Link>
-					</div>
 				</div>
 			</div>
 
-			<div className='px-6 mt-3 text-center'>
-				<h1 className='bg-gradient-to-br from-[#580663] via-[#a324af] to-[#f42b2b] text-transparent bg-clip-text text-[50px]'>Build your next commerce stack</h1>
-				<p className='mt-5 text-lg text-gray-400 px-[120px] lg:mt-8'>A community curated list of commerce products, services, podcasts, books, and more. A heads-up for modern store builders.</p>
-				<button className='mt-6 text-gray-600 hover:text-black lg:mt-10'>Learn more</button>
+			<div className='px-6 mt-3 text-center tracking-tight md:py-[100px]'>
+				<h1 className='bg-gradient-to-br from-[#580663] via-[#a324af] to-[#f42b2b] text-transparent bg-clip-text font-bold text-[25px] md:px-10 md:text-7xl'>Build your next commerce stack</h1>
+				<p className='mt-5 text-base text-gray-400 md:px-[40px] md:text-xl lg:mt-8'>A community curated list of commerce products, services, podcasts, books, and more. A heads-up for modern store builders.</p>
+				<button className='mt-6 text-gray-500 hover:text-black md:text-sm'>Learn more</button>
 			</div>
 
 		</div>
--- components/Tools.jsx
+++ components/Tools.jsx
@@ -8,7 +8,7 @@ const Tools = () => {
 
 	  const tool = tools.map(tool =>
 		<div className=''>
-			<div className='group bg-[#fff] border relative border-gray-200 text-gray-400 p-7 rounded-lg justify-center my-5 hover:bg-gradient-to-r from-red-600 to-purple-500 w-[100%] h-[100%] '>
+			<div className='group shadow-sm bg-[#fff] border relative border-gray-200 text-gray-400 p-7 rounded-lg justify-center hover:bg-gradient-to-r from-red-600 to-purple-500 w-[100%] h-[100%]'>
 	
 					<Link href={tool.url}>	
 						<a href="#" className='absolute top-3 right-3'>
@@ -16,15 +16,15 @@ const Tools = () => {
 								<span className='rounded-xl group-hover:bg-transparent bg-gray-100 text-gray-400 text-[12px] px-2 py-1 ml-2 '>{target}</span>))}
 						</a>
 						
-						<div className='flex flex-col items-center mt-5'>
+						<div className='flex flex-col items-center mt-7'>
 							<img className='w-[10rem] h-[5rem] rounded-lg' 
 								src={tool.icon}
 								alt={tool.title}
 							/>
 							
 							<div className='flex flex-col items-center'>
-								<p className='text-[#000] font-[600] text-xl mb-3'>{tool.title}</p>
-								<p className='flex max-w-[50rem]'>{tool.text}</p>
+								<p className='text-[#000] font-[600] text-xl my-3'>{tool.title}</p>
+								<p className='text-sm text-center'>{tool.text}</p>
 							</div>
 
 						</div>
@@ -37,7 +37,7 @@ const Tools = () => {
 			</div>
 		</div>
 	  );
-	  return <ul className='lg:w-[100%] grid gap-4 grid-cols-3'>{tool}</ul>;
+	  return <ul className='grid gap-4 grid-cols-1 p-6 md:grid-cols-2 md:gap-7'>{tool}</ul>;
 	}
 export default Tools;
 
